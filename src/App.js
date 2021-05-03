79eb8be53e6aef146c6ab6b3de03f21f34fd98c1
jiee <jee.vvvv@gmail.com>
2021-05-03 15:50:19 +0900

    componentDidMount - state.isLoading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component{
+	state = {
+		isLoading: true,
+		movies: []
+	};
+	componentDidMount(){
+		setTimeout(() => {
+			this.setState({ isLoading: false });
+		}, 5000)
+	}
+	render(){
+		const { isLoading } = this.state;
+		return (
+			<div>
+				{isLoading ? "Loading.." : "We are Ready"}
+			</div>
+		)
+	}
 }
 
 export default App;
