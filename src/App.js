9d55c9a6e6ad568e0d905abb0ebc87d6e61362cc
jiee <jee.vvvv@gmail.com>
2021-05-04 15:46:34 +0900

    영화정보 api연결, style적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie"
+import "./App.css";
 
 class App extends React.Component{
 	state = {
 		isLoading: true,
 		movies: []
 	};
+	getMovies = async () => {
+		const {data: {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+		//console.log(movies);
+		this.setState({ movies, isLoading: false }) //this.setState({movies: movies})
+	}
 	componentDidMount(){
-		setTimeout(() => {
-			this.setState({ isLoading: false });
-		}, 5000)
+		this.getMovies();
 	}
 	render(){
-		const { isLoading } = this.state;
+		const { isLoading, movies } = this.state;
 		return (
-			<div>
-				{isLoading ? "Loading.." : "We are Ready"}
-			</div>
+			<section className ="container">
+				{isLoading
+					? (<div className="loader">
+							<span className="loader__text">Loading...</span>
+						</div>
+					) : (
+						<div className="movies">
+							{
+								movies.map((movie) => (
+									<Movie
+										key={movie.id}
+										id={movie.id}
+										year={movie.year}
+										title={movie.title}
+										summary={movie.summary}
+										poster={movie.medium_cover_image} 
+										genres={movie.genres}
+									/>
+								))
+							}
+						</div>
+					)
+				}
+			</section>
 		)
 	}
 }
